7a9eb3bad1f12e5796657cc032d1c2a3b9aaf181
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2024-11-23 19:27:22 +0600

    Add: balance information

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,25 @@
 const Home = () => {
-  return <div className="previewLayout">Welcome!</div>;
+  return (
+    <div className="previewLayout">
+      <h1>Welcome! to ASFCS</h1>
+      <h5 className="mb-0">
+        BKash Payment: <span className="balanceCount">01303565316</span>
+      </h5>
+      <h5 className="mb-1 mt-1">
+        BKash & Nagad Send Money:{" "}
+        <span className="balanceCount">01784135726</span>
+      </h5>
+      {/* <h5 className="mb-4 mt-1"> */}
+      <span
+        className="c-red fw-bold mb-4 d-flex"
+        style={{ backgroundColor: "#7767130" }}
+      >
+        বিঃদ্রঃ: সর্বনিম্ন ৫০ টাকা রিচার্জ করতে হবে। ট্রানজেকশন নম্বর এবং মোবাইল
+        নম্বর ভুল হলে লেনদেন গ্রহণ করা হবে না এবং জরিমানাও করা হতে পারে।
+        ব্যালেন্স এড করতে নিচের প্রোফাইল মেনু ভিজিট করুন।
+      </span>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/common/RechargeModal.jsx
+++ src/components/common/RechargeModal.jsx
@@ -70,6 +70,21 @@ const RechargeModal = ({ onClose, open = true }) => {
     >
       <ModalHeader>You want to recharge?</ModalHeader>
       <ModalContent>
+        <h5 className="mb-0">
+          BKash Payment: <span className="balanceCount">01303565316</span>
+        </h5>
+        <h5 className="mb-1 mt-1">
+          BKash & Nagad Send Money:{" "}
+          <span className="balanceCount">01784135726</span>
+        </h5>
+        <span
+          className="c-red fw-bold mb-4 d-flex"
+          style={{ backgroundColor: "#7767130" }}
+        >
+          বিঃদ্রঃ: সর্বনিম্ন ৫০ টাকা রিচার্জ করতে হবে। ট্রানজেকশন নম্বর এবং
+          মোবাইল নম্বর ভুল হলে লেনদেন গ্রহণ করা হবে না এবং জরিমানাও করা হতে
+          পারে।
+        </span>
         <AsForm control={control} errors={errors} size="large">
           <AsSelect
             name="type"
