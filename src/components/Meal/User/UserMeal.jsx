53c450b8e9db2df631236e9ad23aed8f0df5c1fe
shahinahmed6057@gmail.com <mdshahinahmed605701@gmail.com>
2025-04-13 19:37:31 +0600

    update: menu type

--- src/components/Meal/User/UserMeal.jsx
+++ src/components/Meal/User/UserMeal.jsx
@@ -94,7 +94,7 @@ const UserMeal = () => {
           onClick={() => setMealType("সকাল")}
           fluid
         >
-          সেহরি
+          সকাল
         </Button>
         <Button
           primary={mealType === "দুপুর"}
@@ -102,7 +102,7 @@ const UserMeal = () => {
           className="mx-3"
           fluid
         >
-          ইফতার
+          দুপুর
         </Button>
         <Button
           primary={mealType === "রাত"}
